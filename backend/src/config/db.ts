8080e8d83fb544d05a303ebdde0c77af16c90440
B45ittu <128131220+B45ittu@users.noreply.github.com>
2025-07-03 21:59:09 +0530

    Update db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,8 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://snehalelkiwar618:jyjBFHkdUQZxL3VD@cluster0.q4qk4jj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
+const MONGO_URI = process.env.MONGO_URI;
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
